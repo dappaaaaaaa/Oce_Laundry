eb20c15eeac335394e82d8fedfba40fb366d52b4
dappaaaaaaa <daffafebriyan45@gmail.com>
2026-04-30 21:26:15 +0700

    tes lagi

--- Frontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Frontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,8 @@ import Foundation
 
 import file_selector_macos
 import flutter_secure_storage_macos
-import path_provider_foundation
-import rive_common
+import print_bluetooth_thermal
+import rive_native
 import shared_preferences_foundation
 import sqflite_darwin
 import url_launcher_macos
@@ -17,8 +17,8 @@ import webview_flutter_wkwebview
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
+  PrintBluetoothThermalPlugin.register(with: registry.registrar(forPlugin: "PrintBluetoothThermalPlugin"))
+  RiveNativePlugin.register(with: registry.registrar(forPlugin: "RiveNativePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
